4ab167e0bf27afe5edfb0060dea4e882185c99b1
nijiehaha <1076150981@qq.com>
2018-09-04 14:54:53 +0800

    Add files via upload

---
+++ imageVerbTool.py
@@ -0,0 +1,68 @@
+from PIL import Image
+
+def CalculationHeightWidth(imageList):
+
+    targetH, targetW = (0, 0)
+
+    for path in imagelist:
+
+        im = Image.open(path)
+
+        w, h = im.size
+
+        targetW = w
+        targetH = targetH + h
+
+    return (targetW, targetH)
+
+
+
+def verbImage(imageList):
+
+    size = CalculationHeightWidth(imagelist)
+
+    targetIm = Image.new('RGB', size)
+
+    y = 0
+
+    for path in imagelist:
+
+        i = imagelist.index(path)
+
+        im1 = Image.open(path)
+
+        targetIm.paste(im1, (0, y))
+
+        h = im1.size[1]
+
+        y = y + h
+
+    targetIm.save('myLongImage.jpg')
+
+    print('路小飞，您的图片myLongImage拼接完成！！')
+
+# def CalculationHeightWidth1(imageList):
+#
+#     list = []
+#
+#     for path in imagelist:
+#
+#         im = Image.open(path)
+#
+#         w, h = im.size
+#
+#         list.append((w, h))
+#
+#     return list
+
+imagelist = ['test1.jpg', 'test2.jpg', 'test3.jpg']
+
+list = verbImage(imagelist)
+
+print(list)
+
+# verbImage(imagelist)
+
+
+
+
